e07c60fd260d3735885e5ac06ebcda56a922d42f
Ope Dare <dreoly57@gmail.com>
2022-11-03 19:06:40 -0600

    s3 removed

--- ec2.tf
+++ ec2.tf
@@ -11,12 +11,3 @@ resource "aws_instance" "webinstance" {
     Name = "airbnb-appserver-instance"
   }
 }
-
-resource "aws_s3_bucket" "b" {
-  bucket = "herry-451987"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
