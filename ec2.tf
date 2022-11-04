842b26f4befc77a29dd29b374490381c34f86f80
Ope Dare <dreoly57@gmail.com>
2022-11-03 18:20:04 -0600

    ec2 ammended

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,13 @@
 resource "aws_instance" "webinstance" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  monitoring = true
+  root_block_device {
+    encrypted = true
+    
+  }
 
   tags = {
-    Name = "airbnb-webserver"
+    Name = "airbnb-webserver-instance"
   }
 }
